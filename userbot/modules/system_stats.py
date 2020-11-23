1894ada145fef2bb67f80174edd891dbedd6b126
Izumi <65103300+IzumiProject@users.noreply.github.com>
2020-11-23 12:03:21 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -229,17 +229,17 @@ async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     uptime = await get_readable_time((time.time() - StartTime))
     output = (
-        "`Bot services is running...`\n"
-        "`⊷⊷⊷⊷⊷⊷⊷⊷⊷⊷⊶⊷⊶⊶⊶⊶⊶⊶⊶⊶⊶⊶`\n"
-        f"•  ⚙️ `Telethon       : v{version.__version__} `\n"
-        f"•  🐍 `Python         : v{python_version()} `\n"
-        f"•  👤 `User           :`  {DEFAULTUSER} \n"
-        "`----------------------------------`\n"
-        f"•  💻 `Running on     : {repo.active_branch.name} `\n"
-        f"•  🗃 `Loaded modules : {len(modules)} `\n"
-        f"•  🧸 `One4uBot       : v{USERBOT_VERSION} `\n"
-        f"•  🕒 `Bot Uptime     : {uptime} `\n"
-        "`⊷⊷⊷⊷⊷⊷⊷⊷⊷⊷⊶⊷⊶⊶⊶⊶⊶⊶⊶⊶⊶⊶`"
+        "**USERBOT** `services is running...`\n"
+        "**⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓**\n"
+        f"•  ⚙️ **Telethon       :** `v{version.__version__} `\n"
+        f"•  🐍 **Python         :** `v{python_version()} `\n"
+        f"•  👤 **User           :**  **{DEFAULTUSER}** \n"
+        "**⊷⊷⊷⊷⊷⊷⊷⊷⊷⊷⊶⊷⊶⊶⊶⊶⊶⊶⊶⊶⊶⊶**\n"
+        f"•  💻 **Running on     :** `{repo.active_branch.name} `\n"
+        f"•  🗃 **Loaded modules :** `{len(modules)} `\n"
+        f"•  🧸 **USERBOT       :** `v{USERBOT_VERSION} `\n"
+        f"•  🕒 **Bot Uptime     :** `{uptime} `\n"
+        "**⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓⎓**"
     )
     if ALIVE_LOGO:
         try:
